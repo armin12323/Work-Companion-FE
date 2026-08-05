7b5af54fc36277aa72da534306b37a6bb47e6a90
armin12323 <Armink499@gmail.com>
2026-08-05 01:46:43 -0400

    signed out page, redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { Show, SignInButton, SignUpButton, UserButton, ClerkProvider, useUser } from '@clerk/react'
 import { MainNotesView } from './components/main-notes-view'
-
+import "./index.css"
 
 
 export default function App() {
 
   return (
     <>
-      <div>
+      <div id='sign-in-div'>
 
         <header>
-        <Show when="signed-out">
-          <center>
-            <SignInButton />
-            <SignUpButton />
-          </center>
+          <Show when="signed-out">
+            <div id='signed-out-div' className='sign-in-div'>
+              <center>
+                <div>Sign in if you have an account, twerp.</div>
+                <SignInButton />
+                <div>If you arent signed up, wtf?</div>
+                <SignUpButton />
+              </center>
+            </div>
+            
+            
+          </Show>
           
-        </Show>
-        <Show when="signed-in">
-          <UserButton appearance={{
-                      elements: {
-                        rootBox: {
-                          position: "fixed",
-                          right: 20
+          <Show when="signed-in">
+            <UserButton appearance={{
+                        elements: {
+                          rootBox: {
+                            position: "fixed",
+                            right: 20
+                          },
                         },
-                      },
-                    }}/>
-          <h1>Work Companion</h1>
-          <MainNotesView />
-        </Show>
-      </header>
+                      }}/>
+            <h1>Work Companion</h1>
+            <MainNotesView />
+          </Show>
+        </header>
 
       </div>
     </>
